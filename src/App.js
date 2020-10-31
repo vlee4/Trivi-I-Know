5d454923c154a1220476afbcac2b8d4974e073f8
Victoria <vicglee1234007@gmail.com>
2020-10-31 00:27:28 -0500

    Created function to choose 10 random questions from question file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 // import Start from "./Components/Start";
-// import Question from "./Components/Question"
-import Results from "./Components/Results";
+import Question from "./Components/Question"
+// import Results from "./Components/Results";
+import QandA from "./Apprentice_TandemFor400_Data.json";
 
 function App() {
+ //Chose questions for the round
+  function choose(QandA){
+    let chosen = [];
+    let counter = 0;
+    while(counter<10){
+      let index = Math.floor(Math.random()*(QandA.length))
+      chosen.push(QandA[index]);
+      counter++;
+    }
+    console.log(chosen);
+    return chosen;
+  }
+  choose(QandA)
+
+
   return (
     <div className="App">
       <h1 id="gameName">Trivi-I-Know</h1>
       {/* <Start/> */}
-      {/* <Question/> */}
-      <Results/>
+      <Question/>
+      {/* <Results/> */}
     </div>
   );
 }
