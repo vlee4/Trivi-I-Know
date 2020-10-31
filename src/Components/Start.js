6c52bad2dd41e361eeb21ca067d6b98bf624bfb9
Victoria <vicglee1234007@gmail.com>
2020-10-30 22:18:11 -0500

    Changed title, Added App & Start styling, Created main content for start menu

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function Start(){
   return(
-    <div>Start Screen Placeholder</div>
+    <div className="start">
+      <div>Welcome to Trivi-I-Know</div>
+      <div>Score board would go here</div>
+      <div className="playerInput">
+      <label htmlFor="name">Player Name: </label>
+      <input name="name" placeholder="Please enter your name"/>
+      <button>Start Game</button>
+      </div>
+     </div>
   )
 }
 
