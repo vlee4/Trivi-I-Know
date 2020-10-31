a885512a9d34f33bf891f8aac38262486042613a
Victoria <vicglee1234007@gmail.com>
2020-10-30 22:47:03 -0500

    Radio buttons only allow selection of one answer choice

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -12,23 +12,21 @@ function Question(){
       <form className="playerInput">
         <h3>Here's your question?</h3>
         <div className="answer">
-          <input name="A1" type="radio" placeholder="Create map function to display question's 4 answers"/>
+          <input name="answer" type="radio" placeholder="Create map function to display question's 4 answers"/>
           <label htmlFor="A1">Answer 1</label>
         </div>
         <div className="answer">
-          <input name="A2" type="radio" placeholder="Create map function to display question's 4 answers"/>
+          <input name="answer" type="radio" placeholder="Create map function to display question's 4 answers"/>
           <label htmlFor="A1">Answer 2</label>
         </div>
         <div className="answer">
-          <input name="A3" type="radio" placeholder="Create map function to display question's 4 answers"/>
+          <input name="answer" type="radio" placeholder="Create map function to display question's 4 answers"/>
           <label htmlFor="A1">Answer 3</label>
         </div>
         <div className="answer">
-          <input name="A4" type="radio" placeholder="Create map function to display question's 4 answers"/>
+          <input name="answer" type="radio" placeholder="Create map function to display question's 4 answers"/>
           <label htmlFor="A1">Answer 4</label>
         </div>
-
-
           <button type="submit">Submit</button>
       </form>
 
