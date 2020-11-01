ebb8a968b9a6489ff3a2e0d2dec686c14560af73
Victoria <vicglee1234007@gmail.com>
2020-11-01 16:47:47 -0600

    Fiddling with footer logos

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     if(!this.props.gameStats.phase){
       return(
         <div className="App">
+          <h1 id="gameName">Trivi-I-Know</h1>
         <h2 className="loading">Loading...</h2>
       </div>
     )
@@ -21,6 +22,15 @@ class App extends React.Component {
       <div className="App">
       <h1 id="gameName">Trivi-I-Know</h1>
       {phase==="start"? (<Start/>):(phase==="questions"?(<Question/>):(<Results/>))}
+      <footer>
+        <div>
+          <div>Last Updated: November 2020</div>
+          <div><img src={"./GitHub-Mark-32px.png"} alt="GH_Logo"></img><a href="https://github.com/vlee4/Trivi-I-Know"><img src={'./public/GitHub_Logo_White.png'} alt=
+          "Github"></img></a></div>
+
+          <div>Created by vLEE</div>
+        </div>
+        </footer>
       </div>
       );
     }
