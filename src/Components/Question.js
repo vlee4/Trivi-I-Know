79c7cdd18253aef53ce920027cbace023d103474
Victoria <vicglee1234007@gmail.com>
2020-10-31 22:19:50 -0500

    Reformat form of question sent from redux store, adding props redux store to Question component

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,17 +1,27 @@
 import React from "react";
+import {connect} from "react-redux";
+import {formatQuestion} from "../Store/questionsReducer";
 
-function Question(){
+class Question extends React.Component{
+  componentDidMount(){
+    console.log("PROPS", this.props)
+    // let qNum = (this.props&&this.props.)
+    // this.props.getQ()
+  }
+  render(){
   return(
     <div className="question">
       <div className="topBanner">
-      <div>Question #/10</div>
+  <div>Question {this.props.QNum+1}/10</div>
       <div className="navQuestions">
         <button>Previous</button>
         <button>Next</button>
         </div></div>
+
       <form className="playerInput">
-        <h3>Here's your question?</h3>
+        <h3 className="questionHeader">Here's your question?</h3>
         <div className="answer">
+
           <input name="answer" type="radio" placeholder="Create map function to display question's 4 answers"/>
           <label htmlFor="A1">Answer 1</label>
         </div>
@@ -29,10 +39,23 @@ function Question(){
         </div>
           <button type="submit">Submit</button>
       </form>
-
-
     </div>
   )
+  }
 }
 
-export default Question;
+const mapStateToState = (state) => {
+  return {
+    curQuestion: state.questions.CurQ,
+    QNum: state.questions.QNum,
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getQ: (qNum) => dispatch(formatQuestion(qNum))
+  }
+}
+
+export default connect(mapStateToState, mapDispatchToProps)(Question);
--- src/Store/questionsReducer.js
+++ src/Store/questionsReducer.js
@@ -1,7 +1,14 @@
 import QandA from "../Apprentice_TandemFor400_Data.json"
+//Initial State
+const initialState = {
+  QNum: 0,
+  CurQ: {},
+  Questions: [],
+}
 
 //Action
 const GET_QUESTIONS = "GET_QUESTIONS";
+const GENERATE_QUESTION = "GENERATE_QUESTION";
 
 //ACTION CREATOR
 const getQuestions = (questions) => {
@@ -11,32 +18,63 @@ const getQuestions = (questions) => {
   }
 }
 
-export const pickQuestions = () => {
-  return (dispatch) => {
-    let chosen = [];
-    let counter = 0;
-    while(counter<10){
-      let index = Math.floor(Math.random()*(QandA.length))
-      chosen.push(QandA[index]);
-      counter++;
-    }
-    console.log("CHOSEN",chosen);
-    dispatch(getQuestions(chosen));
+const generateQuestion = (question) => {
+  return {
+    type: GENERATE_QUESTION,
+    question
   }
 }
 
-/*State = {
-  QNum: 0,
-  CurQ: {},
-  Questions: [],
+export const pickQuestions = () => {
+  return (dispatch) => {
+    let chosen = new Set();
+    let counter = 0;
+    while(counter<10){
+      let index = Math.floor(Math.random()*(QandA.length));
+      if(!chosen.has(index)){
+        chosen.add(index);
+        counter++;
+      }
+    }
+    console.log("CHOSEN",[...chosen]);
+    dispatch(getQuestions([...chosen]));
+  }
 }
-*/
+//Input will be INDEX in QandA to look for
+export const formatQuestion = (qNum) => {
+  return (dispatch) => {
+    let {question, correct, incorrect} = QandA[qNum];
+    let inserts = new Set();
+    let answers = [correct, ...incorrect];
+
+    //Randomize order of possible answers
+    while(inserts.size<4){ //creates order of indexes
+      let idx = Math.floor(Math.random()*(3))
+      inserts.add(idx);
+    }
+    let ansArr = [];
+    inserts.forEach(idx=> {
+      ansArr.push(answers[idx])
+    })
+
+   console.log("ANSWERS", ansArr)
+    dispatch(generateQuestion({question, answers: ansArr}))
+  }
+}
+/*{
+  queston: q,
+  answers: [in randomized order]
+
+}*/
+
+
 //Reducer
-export default function questionsReducer(state={QNum: 0, CurQ: {}, Questions: []}, action){
+export default function questionsReducer(state=initialState, action){
   switch(action.type){
     case GET_QUESTIONS:
-      let first = action.questions[0];
-      return {...state, CurQ: first, Questions: action.questions}
+      return {...state, Questions: action.questions}
+    case GENERATE_QUESTION:
+      return {...state, CurQ: action.question}
     default:
       return state;
   }
